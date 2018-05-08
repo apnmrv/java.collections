3c9682653b5a91bc54cc4ef4f7e5ff3ae56bea76
apnmrv <apnmrv.gmail.com>
2018-05-08 20:23:03 +0300

    изменено:      Ex02.1_Set/src/main/java/ru/apnmrv/java/collections/App.java

--- Ex02.1_Set/src/main/java/ru/apnmrv/java/collections/App.java
+++ Ex02.1_Set/src/main/java/ru/apnmrv/java/collections/App.java
@@ -1,9 +1,7 @@
 package ru.apnmrv.java.collections;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 /**
@@ -12,44 +10,22 @@ import java.util.Set;
  */
 public class App 
 {
+    private static Collection<?> collection;
     private static Collection<?> noDubbles;
 
     public static void main( String[] args )
     {
+        collection = Arrays.asList(args).stream()
+            .collect(Collectors.toList());
+        printCollection(collection);
 
-        Collection<Integer> cInt = new ArrayList<Integer>();
-        Collection<String> cStr = new ArrayList<String>();
-
-        Integer[] arrInt = {1, 2, 3, 3, 3, 3, 5, 5, 6, 7, 8, 8, 89};
-        String[] arrStr = {"wer","wer","xcv","ssdf","xcv","ssdf","ghj","ghj","gjh","ghj","ghj","ghj"};
-
-        for (int i:arrInt){
-            cInt.add(i);
-        }
-
-        for (String s:arrStr){
-            cStr.add(s);
-        }
-
-        for (int num:cInt) System.out.printf("%d ", num);
-        System.out.println("\n");
-
-        for (String s:cStr) System.out.printf("%s ", s);
-        System.out.println("\n");
-
-        noDubbles = removeDubbles(cInt);
+        noDubbles = removeDubbles(collection);
         printCollection(noDubbles);
-
-        noDubbles = removeDubbles(cStr);
-        printCollection(noDubbles);
-
-//        noDubbles = new HashSet<Integer>(cInt); // Works too
-//        printCollection(noDubbles);
 
     }
 
     private static <E> Set<E> removeDubbles(Collection<E> cStr) {
-        System.out.println("No more dubbles!");
+        System.out.println("No more dublets!");
         return new HashSet<E>(cStr);
     }
 
