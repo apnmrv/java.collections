83276a4fc924a2865a267be968e1dfdaff0a3e02
apnmrv <apnmrv@gmail.com>
2018-05-09 12:23:32 +0300

    tests added

--- Ex03.1_List/src/main/java/ru/apnmrv/java/collections/App.java
+++ Ex03.1_List/src/main/java/ru/apnmrv/java/collections/App.java
@@ -1,13 +1,32 @@
 package ru.apnmrv.java.collections;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] strArgs, Integer[] intArgs )
     {
         System.out.println( "Hello World!" );
+
+        List<?> list = getArrayList(strArgs);
+    }
+
+    public static <T> List<T> getArrayList(T [] els) {
+        List<T> list = new ArrayList<T>();
+        list.addAll(Arrays.asList(els));
+        return list;
+    }
+
+    public static <T> List<T> getLinkedList(T [] els) {
+        List<T> list = new LinkedList<T>();
+        list.addAll(Arrays.asList(els));
+        return list;
     }
 }
--- Ex03.1_List/src/test/java/ru/apnmrv/java/collections/AppTest.java
+++ Ex03.1_List/src/test/java/ru/apnmrv/java/collections/AppTest.java
@@ -3,12 +3,14 @@ package ru.apnmrv.java.collections;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+
+import java.util.List;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestCase
 {
     /**
      * Create the test case
@@ -34,5 +36,42 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+
+    public void testGetArrayList()
+    {
+        System.out.println("Testing method getArrayList()...");
+        String [] sArr = {"String1", "String2"};
+        List<String> stringList = App.getArrayList(sArr);
+
+        testListContainsElements(stringList, sArr);
+        testNotEmptyList(stringList);
+        testListDoNotContainsElement(stringList);
+
+    }
+
+    public void testLinkedList()
+    {
+        System.out.println("Testing method getLinkedList()...");
+        String [] sArr = {"String1", "String2"};
+        List<String> stringList = App.getLinkedList(sArr);
+
+        testListContainsElements(stringList, sArr);
+        testNotEmptyList(stringList);
+        testListDoNotContainsElement(stringList);
+    }
+
+    private void testNotEmptyList(List<?> list) {
+        assertTrue(!list.isEmpty());
+    }
+
+    private <T> void testListContainsElements(List<T> list, T [] els){
+        for(T e : els){
+            assertTrue( list.contains(e) );
+        }
+    }
+
+    private <T> void testListDoNotContainsElement(List<T> list){
+        assertTrue(!list.contains("StrangerThings"));
     }
 }
